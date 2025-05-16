08c7743c923f1491f2cf8d2b7655f9b99ca15487
Greg Magarshak <greg@qbix.com>
2025-05-16 18:33:19 -0400

    Added hook to Users_logout to log out discourse

--- handlers/Communities/after/Users_logout.php
+++ handlers/Communities/after/Users_logout.php
@@ -3,4 +3,23 @@
 function Communities_after_Users_logout()
 {
 	Q_Response::clearCookie('Q_Users_communityId');
+
+	$secret = Q_Config::get("Communities", "Discourse", "SSO", "secret", null);
+	if (!$secret) {
+		return;
+	}
+	$returnUrl = Q_Request::baseUrl('/Q/blank');
+	$params = http_build_query(array(
+		'return_url' => $returnUrl
+	));
+	$payload = base64_encode($params);
+	$sig = hash_hmac('sha256', $payload, $secret);
+	Users::$logoutFetch['Communities'] = array(
+		'method' => 'GET',
+		'url' => "https://forum.miracles.community/session/sso_logout",
+		'fields' => array(
+			'sso' => $payload,
+			'sig' => $sig
+		)
+	);
 }
