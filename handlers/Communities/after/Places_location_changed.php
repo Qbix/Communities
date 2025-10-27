e931aab2983b73b66438348acfc3720e5f0b8fbf
Greg Magarshak <greg@qbix.com>
2025-10-27 17:29:12 -0400

    Added chr() to ord()+1

--- handlers/Communities/after/Places_location_changed.php
+++ handlers/Communities/after/Places_location_changed.php
@@ -8,10 +8,13 @@ function Communities_after_Places_location_changed($params)
 	// if location just created, join user to nearest community
 	if ($created) {
 		$q = Places_Location::select()->where(array(
-			'publisherId' => new Db_Range('A', true, true, 'Z'),
+			'publisherId' => new Db_Range('A', true, false, chr(ord('Z')+1)),
 			'streamName like ' => 'Places/location/%'
 		));
-		$res = Places_Geohash::fetchByDistance($q, 'geohash', Places_Geohash::encode($stream->getAttribute('latitude'), $stream->getAttribute('longitude')), 1);
+		$res = Places_Geohash::fetchByDistance($q, 'geohash', Places_Geohash::encode(
+			$stream->getAttribute('latitude'), 
+			$stream->getAttribute('longitude')
+		), 1);
 
 		if (!empty($res)) {
 			$row = reset($res);
