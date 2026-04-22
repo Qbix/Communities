53a454b913ceacfac21efde3db4b2a47b04bff08
Greg Magarshak <greg@qbix.com>
2026-04-21 23:11:36 -0400

    Calling dashboardMenu

--- handlers/Q/response/dashboard.php
+++ handlers/Q/response/dashboard.php
@@ -6,13 +6,14 @@ function Q_response_dashboard()
 	$dashboardStyle = Q_Config::get(
 		'Communities', 'layout', 'dashboard', Q_Request::formFactor(), 'contextual'
 	);
+	$dashboardMenu = Communities::dashboardMenu();
 	$withTitles = Q_Config::get('Communities', 'layout', 'dashboard', 'withTitles', false);
 	$attributes = array();
 	if ($avatar = Streams_Avatar::fetch(null, Users::communityId())) {
 		$avatar->addPreloaded();
 	}
 	$defaultTabName = null;
-	return Q::view('Communities/dashboard.php', @compact(
+	return Q::view('Communities/dashboard.php', array_merge(@compact(
 		'isMobile', 'dashboardStyle', 'withTitles', 'attributes', 'defaultTabName'
-	));
+	), $dashboardMenu));
 }
