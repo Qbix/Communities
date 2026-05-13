3c5b911affd82428593d476a1d321ac9081b842b
Andrey Tepaykin <andrey@qbix.com>
2026-05-13 15:57:58 +0300

    With Q.c param check, also check the invitation token. If the user is invited to a community different from the current community, switch the community.

--- handlers/Communities/before/Q_reroute.php
+++ handlers/Communities/before/Q_reroute.php
@@ -3,17 +3,34 @@
 function Communities_before_Q_reroute()
 {
 	$communityId = Q_Request::special('c', null);
-	if ($communityId) {
-		if (!Users::isCommunityId($communityId)) {
-			throw new Q_Exception_WrongValue(array(
-				'field' => 'c',
-				'range' => 'The ID of a community'
-			));
+	if (!$communityId) {
+		$token = Q_Dispatcher::uri()->token;
+		if ($token) {
+			$invite = Streams_Invite::fromToken($token);
+			if ($invite && Users::isCommunityId($invite->publisherId) && $invite->publisherId != Users::currentCommunityId(true)) {
+				$communityId = $invite->publisherId;
+			}
 		}
-		Users_User::fetch($communityId, true);
-		Q_Response::setCookie('Q_Users_communityId', $communityId, time()+60*60*24*365);
-		Communities::setCommunity($communityId, array(
-			'subscribe' => array('Streams/experience/main')
+	}
+
+	if (!$communityId) {
+		return;
+	}
+
+	if (!Users::isCommunityId($communityId)) {
+		throw new Q_Exception_WrongValue(array(
+			'field' => 'c',
+			'range' => 'The ID of a community'
 		));
 	}
+
+	if (Users::currentCommunityId(true) == $communityId) {
+		return;
+	}
+
+	Users_User::fetch($communityId, true);
+	Q_Response::setCookie('Q_Users_communityId', $communityId, time()+60*60*24*365);
+	Communities::setCommunity($communityId, array(
+		'subscribe' => array('Streams/experience/main')
+	));
 }
